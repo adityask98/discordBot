0e679866e82f7f1d644b1e65d251ac1ead98d2dd
Aditya Srinivasa <adityask98@gmail.com>
2024-05-17 17:28:52 +0900

    removed token

--- Sources/App/BotGatewayManager.swift
+++ Sources/App/BotGatewayManager.swift
@@ -5,7 +5,7 @@ import AsyncHTTPClient
 
 // let httpClient = HTTPClient(eventLoopGroupProvider: )
 // let bot = await BotGatewayManager(
-//     token: "MTI0MDU1MTEzOTA2MjA1OTA2OQ.G9-rVv.vEremW4BbLLqIKjrTZhJgo7F6-HKDQFzvosnck", 
+//     token: "random", 
 //     presence: .init(activities: [.init(name: "SwiftBot", type: .game)], status: .online, afk: false),
 //     intents: [.guildMessages, .messageContent]
 //     )
